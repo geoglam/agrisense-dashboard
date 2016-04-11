d35ac43f6f81fbeda74aebec73cc82496f04ab95
Jon Nordling <jonnordling@jonnordling.local>
2016-04-10 23:19:40 -0400

    Updating app

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,344 +1,182 @@
-require(["leaflet", "dojo/domReady!"], function(L) {
-	 $(document).ready(function() {
-
-
-$('select').material_select();
-		var siteObject = {
-		url:'http://data.geomarvel.io/api/v1/',
-		token:'ssfdk343434'
-		}
-		var onaClient = new OnaClient(siteObject);
+require([], function() {
+	var onaClient;
+	//agrisense
+	//Jambula
+	function getAllForms(){
 		onaClient.getForms(function(data){
 			var html = '';
 			for (var i = 0; i < data.length; i++) {
 				html += "<option>"+data[i].title+"</option>";
 			}
 			$("#formlist").html(html);
-			$('select').material_select();
-
+			resetMasterialSelect();
 		});
-    $(".button-collapse").sideNav();
-    $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 10, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
-    });
-    // $( "#loading" ).css('display','none');
-    $( "#loading" ).animate({
-    opacity: 0,
-    display:'none',
-  },1000, function() {
-    // Animation complete.
-    this.remove();
-    // this.css('display','none')
-  });
+	}
 
+	function resetMasterialSelect(){
+		$('select').material_select();
+	}
 
+	function stopSplashScreen(){
+		$( "#loading" ).animate({
+				opacity: 0,
+				display:'none',
+			},1000, function() {
+				this.remove();
+		});
+	}
 
-  });
-  var getData = function(parms,callback){
-	$.ajax({
-	  	 "crossDomain": true,
-	      url:"http://data.geomarvel.io/api/v1/forms",
-	      type:"GET",
-	      headers: {
-	        "authorization" : "TempToken "+user.temp_token,
-	      },
-	      data:parms,
-	      dataType:"json"
-	    }).done(function(data){
-	    	callback(data);
-	    });
-  }
-  // var map = new Map("map", {
-  //   center: [-118, 34.5],
-  //   zoom: 8,
-  //   basemap: "gray"
-  // });
-  var map = L.map('map').setView([-6.489983, 35.859375], 6);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+	function addGeoJSON(url, geojson){
+		$.ajax({
+			dataType: "json",
+			url: url,
+			success: function(data) {
+		  	$(data.features).each(function(key, data) {
+		        geojson.addData(data);
+		    });
+			}
+		}).error(function() {});
+	}
+	function districtsChange(value){
+		console.log(value);
+	}
 
+	function setDistrictsItems(url){
+		$.ajax({
+			dataType: "json",
+			url: url,
+			success: function(data) {
+				var html = "<option>All</option>";
+				var districts =[]
+		  	$(data.features).each(function(key, data) {
+						districts.push(data.properties.District);
+						// html += "<option>"+data.properties.District+"</option>";
+		    });
+				districts.sort();
+				// view-source:http://techslides.com/demos/leaflet/leaflet-country-zoom.html
+				$(districts).each(function(key,data){
+					html += "<option value='"+data+"'>"+data+"</option>";
+				})
+				$("#districts").html(html);
+				$('#districts').change(function(){
+					districtsChange(this.value)
+					// actions.changeVillage(this.value)
+				});
+				$('select').material_select();
+			}
+		}).error(function() {});
+	}
+	function initMap(){
+		var map = L.map('map').setView([-6.489983, 35.859375], 6);
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
 
-var district_boundary = new L.geoJson();
-district_boundary.addTo(map);
-$.ajax({
-dataType: "json",
-url: "data/TZregions_2012.geojson",
-success: function(data) {
-    $(data.features).each(function(key, data) {
-        district_boundary.addData(data);
-    });
-}
-}).error(function() {});
-  var data = getData({owner:user.username},function(data){
-  	console.log(data);
-  });
+		var district_boundary = new L.geoJson();
+		district_boundary.addTo(map);
+		addGeoJSON("data/TZdistricts_2012.geojson",district_boundary);
+		setDistrictsItems('data/TZdistricts_2012.geojson')
 
+		var end_of_season_assessment = new L.geoJson();
+		end_of_season_assessment.addTo(map);
+		addGeoJSON("data/disctrict_join_end_of_season_assessment.geojson",end_of_season_assessment);
 
+		var in_season_assessment = new L.geoJson();
+		in_season_assessment.addTo(map);
+		addGeoJSON("data/disctrict_join_in_season_assessment.geojson",in_season_assessment);
 
+		var pre_season_assessment = new L.geoJson();
+		pre_season_assessment.addTo(map);
+		addGeoJSON("data/district_join_pre_season_assessment.geojson",pre_season_assessment);
 
+		$.ajax({
+			dataType: "json",
+			url: 'data/disctrict_join_in_season_assessment.geojson',
+			success: function(data) {
+				var crops = []
 
-$('#chart1').highcharts({
-        title: {
-            text: 'Monthly Average Temperature',
-            x: -20 //center
-        },
-        subtitle: {
-            text: 'Source: WorldClimate.com',
-            x: -20
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: {
-            title: {
-                text: 'Temperature (°C)'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            valueSuffix: '°C'
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-        }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-        }, {
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-        }]
-    });
+				$(data.features).each(function(key, data) {
+					crops.push(data.properties.agricultur);
+				});
+				unique_crops = _.uniq(crops);
+				chart_object = []
+				for (var i = 0; i < unique_crops.length; i++) {
 
+					 var obj = {
+						name:unique_crops[i],
+						y:_.countBy(crops)[unique_crops[i]],
+						}
+					chart_object.push(obj);
+				}
 
-    $('#chart2').highcharts({
+				$('#chart1').highcharts({
         chart: {
-            type: 'column'
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
         },
         title: {
-            text: 'Stacked column chart'
-        },
-        xAxis: {
-            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Total fruit consumption'
-            },
-            stackLabels: {
-                enabled: true,
-                style: {
-                    fontWeight: 'bold',
-                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                }
-            }
-        },
-        legend: {
-            align: 'right',
-            x: -30,
-            verticalAlign: 'top',
-            y: 25,
-            floating: true,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-            borderColor: '#CCC',
-            borderWidth: 1,
-            shadow: false
+            text: 'Crop Types'
         },
         tooltip: {
-            headerFormat: '<b>{point.x}</b><br/>',
-            pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
         },
         plotOptions: {
-            column: {
-                stacking: 'normal',
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
                 dataLabels: {
                     enabled: true,
-                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    format: '<b>{point.name}</b>: {point.y}',
                     style: {
-                        textShadow: '0 0 3px black'
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
                     }
                 }
             }
         },
         series: [{
-            name: 'John',
-            data: [5, 3, 4, 7, 2]
-        }, {
-            name: 'Jane',
-            data: [2, 2, 3, 2, 1]
-        }, {
-            name: 'Joe',
-            data: [3, 4, 4, 2, 5]
+            name: 'Crops',
+            colorByPoint: true,
+            data:chart_object
         }]
     });
 
 
 
-    var colors = Highcharts.getOptions().colors,
-        categories = ['MSIE', 'Firefox', 'Chrome', 'Safari', 'Opera'],
-        data = [{
-            y: 56.33,
-            color: colors[0],
-            drilldown: {
-                name: 'MSIE versions',
-                categories: ['MSIE 6.0', 'MSIE 7.0', 'MSIE 8.0', 'MSIE 9.0', 'MSIE 10.0', 'MSIE 11.0'],
-                data: [1.06, 0.5, 17.2, 8.11, 5.33, 24.13],
-                color: colors[0]
-            }
-        }, {
-            y: 10.38,
-            color: colors[1],
-            drilldown: {
-                name: 'Firefox versions',
-                categories: ['Firefox v31', 'Firefox v32', 'Firefox v33', 'Firefox v35', 'Firefox v36', 'Firefox v37', 'Firefox v38'],
-                data: [0.33, 0.15, 0.22, 1.27, 2.76, 2.32, 2.31, 1.02],
-                color: colors[1]
-            }
-        }, {
-            y: 24.03,
-            color: colors[2],
-            drilldown: {
-                name: 'Chrome versions',
-                categories: ['Chrome v30.0', 'Chrome v31.0', 'Chrome v32.0', 'Chrome v33.0', 'Chrome v34.0',
-                    'Chrome v35.0', 'Chrome v36.0', 'Chrome v37.0', 'Chrome v38.0', 'Chrome v39.0', 'Chrome v40.0', 'Chrome v41.0', 'Chrome v42.0', 'Chrome v43.0'
-                    ],
-                data: [0.14, 1.24, 0.55, 0.19, 0.14, 0.85, 2.53, 0.38, 0.6, 2.96, 5, 4.32, 3.68, 1.45],
-                color: colors[2]
-            }
-        }, {
-            y: 4.77,
-            color: colors[3],
-            drilldown: {
-                name: 'Safari versions',
-                categories: ['Safari v5.0', 'Safari v5.1', 'Safari v6.1', 'Safari v6.2', 'Safari v7.0', 'Safari v7.1', 'Safari v8.0'],
-                data: [0.3, 0.42, 0.29, 0.17, 0.26, 0.77, 2.56],
-                color: colors[3]
-            }
-        }, {
-            y: 0.91,
-            color: colors[4],
-            drilldown: {
-                name: 'Opera versions',
-                categories: ['Opera v12.x', 'Opera v27', 'Opera v28', 'Opera v29'],
-                data: [0.34, 0.17, 0.24, 0.16],
-                color: colors[4]
-            }
-        }, {
-            y: 0.2,
-            color: colors[5],
-            drilldown: {
-                name: 'Proprietary or Undetectable',
-                categories: [],
-                data: [],
-                color: colors[5]
-            }
-        }],
-        browserData = [],
-        versionsData = [],
-        i,
-        j,
-        dataLen = data.length,
-        drillDataLen,
-        brightness;
+			}
+		}).error(function() {});
+		  // var data = getData({owner:user.username},function(data){
+		  // 	console.log(data);
+		  // });
+	}
 
+	function init(){
+		resetMasterialSelect();
+		var siteObject = {
+			url:'http://52.23.108.108/api/v1/',
+			token:'ssfdk343434'
+		}
+		onaClient = new OnaClient(siteObject);
 
-    // Build the data arrays
-    for (i = 0; i < dataLen; i += 1) {
+		$('.dropdown-button').dropdown({
+			inDuration: 300,
+			outDuration: 225,
+			constrain_width: false,
+			hover: true,
+			gutter: 10,
+			belowOrigin: true,
+			alignment: 'right'
+		});
 
-        // add browser data
-        browserData.push({
-            name: categories[i],
-            y: data[i].y,
-            color: data[i].color
-        });
+		stopSplashScreen();
+		initMap();
+		// getAllForms();
 
-        // add version data
-        drillDataLen = data[i].drilldown.data.length;
-        for (j = 0; j < drillDataLen; j += 1) {
-            brightness = 0.2 - (j / drillDataLen) / 5;
-            versionsData.push({
-                name: data[i].drilldown.categories[j],
-                y: data[i].drilldown.data[j],
-                color: Highcharts.Color(data[i].color).brighten(brightness).get()
-            });
-        }
-    }
+	}
 
-    // Create the chart
-    $('#chart0').highcharts({
-        chart: {
-            type: 'pie'
-        },
-        title: {
-            text: 'Browser market share, January, 2015 to May, 2015'
-        },
-        subtitle: {
-            text: 'Source: <a href="http://netmarketshare.com/">netmarketshare.com</a>'
-        },
-        yAxis: {
-            title: {
-                text: 'Total percent market share'
-            }
-        },
-        plotOptions: {
-            pie: {
-                shadow: false,
-                center: ['50%', '50%']
-            }
-        },
-        tooltip: {
-            valueSuffix: '%'
-        },
-        series: [{
-            name: 'Browsers',
-            data: browserData,
-            size: '60%',
-            dataLabels: {
-                formatter: function () {
-                    return this.y > 5 ? this.point.name : null;
-                },
-                color: '#ffffff',
-                distance: -30
-            }
-        }, {
-            name: 'Versions',
-            data: versionsData,
-            size: '80%',
-            innerSize: '60%',
-            dataLabels: {
-                formatter: function () {
-                    // display only if larger than 1
-                    return this.y > 1 ? '<b>' + this.point.name + ':</b> ' + this.y + '%' : null;
-                }
-            }
-        }]
-    });
-
-
-
-
-
+	$(document).ready(function() {
+		init();
+	});
 
 });
